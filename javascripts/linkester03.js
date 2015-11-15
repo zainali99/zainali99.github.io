1a9f0b7203e7400a810bae46922fa3d7c50f2a71
Zain Ali <zainali99@users.noreply.github.com>
2015-11-15 16:45:39 +0100

    request the title
    created for only test.

--- javascripts/linkester03.js
+++ javascripts/linkester03.js
@@ -1,9 +1,10 @@
-// Linkester.js (C) 2015 by Zain Ali, jsfiler.altervista.org
-
+// Linkester.js 0.2 (C) 2015 by Zain Ali, http://zainali99.github.io
+//#NOTE:the 03 is created for only test.
 var linkester = (function() {
-var txt, 
-		  cacheToken, element,
-		  o;
+var txt,cacheToken, element, o, d;
+bool=false;
+
+
 	
 
 	function  getHttps() {
@@ -16,12 +17,14 @@ var txt,
 
 	function getText() {
     var linkStart = "<div id='linkester-js-element'>";
+    var titleStart = "<div id='linkester-js-page-title'>";
     var pStart = "<span id='protocol'>"+o.protocol;
     var psStart = "<span id='protocol-secure'>"+o.protocol;
     var hnStart = "<span id='hostname'>//"+o.hostname;
     var pnStart = "<span id='pathname'>"+o.pathname;
     var pmStart = "<span id='protocol-mailto'>"+o.protocol;
     var ftpStart = "<span id='protocol-ftp'>"+o.protocol;
+    
     var closeDiv = "</div>";
     var closeSpan = "</span>";
     
@@ -36,6 +39,19 @@ var txt,
         	txt = linkStart + ftpStart + closeSpan + hnStart + closeSpan + pnStart + closeSpan + closeDiv;
         }
         
+        else if(bool == true) {
+        	//request data: title;
+        	var xhr = new XMLHttpRequest();
+			xhr.open("GET", o.href, true);
+			xhr.onreadystatechange = function() { 
+  			if (xhr.readyState == 4) {
+  				//get title of the page requested.
+    			var title = (/<title>(.*?)<\/title>/m).exec(xhr.responseText)[1];
+    			txt = linkStart + titleStart + title + closeDiv + pStart + closeSpan + hnStart + closeSpan + pnStart + closeSpan + closeDiv;
+  			}
+			}
+			xhr.send();
+        			}
         
 
 		else {
@@ -52,10 +68,17 @@ var txt,
       var obj = where.querySelectorAll("a");
         
 	for(var i=0; i < obj.length; i++){
+		//Are this Important ?!?!
 		obj[i].token = "token:"+i+"_href:"+obj[i];
 		cacheToken= obj[i].token;
 		o = obj[i];
-		element = document.createElement("span");
+		d = document.domain;
+		//the hostname is equal to our domain.
+		if(o.hostname == d) {
+			bool = true;
+
+		}
+
 		getText();
 
 o.insertAdjacentHTML ("afterEnd", txt);
@@ -79,6 +102,6 @@ o.insertAdjacentHTML ("afterEnd", txt);
     
   
 })();
+	
 
 
- 
